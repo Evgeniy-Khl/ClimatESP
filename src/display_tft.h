9d1c8230888f3e9d7430bbed08d377c7dcd21e54
Evgeniy-Khl <graviton.ua@gmail.com>
2025-07-02 08:45:37 +0300

    add display_led; display_tft v0.0

---
+++ src/display_tft.h
@@ -0,0 +1,17 @@
+#ifndef __DISPLAY_H
+#define __DISPLAY_H
+
+#include <main.h>
+
+void mainDispl(void);
+void menu_1(void);
+void menu_2(void);
+void menu_3(void);
+void menu_4(void);
+void calcDisplay(const char* txt);
+void drawKeypad(const char* keyLabel[], uint16_t keyColor[]);
+void drawKeypad_longName_7(const char* keyLabel[], uint16_t keyColor[], uint8_t amt_row, uint8_t amt_col);
+void drawKeypad_longName_12(const char* keyLabel[], uint16_t keyColor[], uint8_t amt_row, uint8_t amt_col);
+uint16_t lampUpdate(uint16_t xpos, uint16_t ypos);
+
+#endif /* __DISPLAY_H */
